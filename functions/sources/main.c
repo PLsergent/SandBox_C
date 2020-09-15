503ba8439e9f492c2205e5adfc714929a17c5e8e
PLsergent <papa.louis59@gmail.com>
2020-09-15 10:53:21 +0200

    Add functions

---
+++ functions/sources/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number, base, output, output_base; 
+    printf("Enter a number : ");
+    scanf("%d", &number);
+
+    printf("Enter the input base (2, 8, 10, 16) : ");
+    scanf("%d", &base);
+    printf("Enter the output base (2, 8, 10, 16) : ");
+    scanf("%d", &output_base);
+
+
+    return 0;
+}
